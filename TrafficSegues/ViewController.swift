a0aa8b677bd96ca2c8816238cf1947929e1fc2e3
Saud Baig <165429@s020rmc148-13.local>
2019-01-28 13:24:21 -0600

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Baig, Saud S on 1/28/19.
+//  Copyright © 2019 Baig, Saud S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
